6c5f39cf837afd01ccde40bdffbd69f9c7ebd6d8
cpp <Ldgr1314>
2017-09-15 14:45:10 +0800

    事件监听

--- src/main/java/com/cpp/web/GlassApplication.java
+++ src/main/java/com/cpp/web/GlassApplication.java
@@ -2,10 +2,14 @@ package com.cpp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class GlassApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(GlassApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(GlassApplication.class);
+        springApplication.addListeners((ApplicationListener<ApplicationReadyEvent>) applicationEvent -> System.out.println("应用启动完成")); //添加启动事件
+        springApplication.run(args);
+    }
 }
